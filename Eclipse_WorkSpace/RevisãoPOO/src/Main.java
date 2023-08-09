d34ab387c5a62097d6e7731803d59f7e2f998eb2
gabrielcaproni <gabrielcaproni09@gmail.com>
2023-08-09 07:21:12 -0300

    Repositorio atualizado

--- Eclipse_WorkSpace/RevisãoPOO/src/Main.java
+++ Eclipse_WorkSpace/RevisãoPOO/src/Main.java
@@ -2,8 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello eclipse world!! ");
-
+		User gabriel = new User();
+		gabriel.setNome("Gabriel");
+		gabriel.setCPF("050");
+		gabriel.addFriend("Ryan");
+		
+		System.out.println("Lista de Amigos \n");
+		for (String friendNome : gabriel.getFriends()) {
+			System.out.println(friendNome);
+		}
 	}
 
 }
