e35e5d20c44e3350005752a58474e03b590a36d2
gabrielcaproni <gabrielcaproni09@gmail.com>
2023-12-05 10:47:56 -0300

    Aula 05/12/2023. Iniciado com apache TomCat com bootStrap e desenvolvimento pra web.

---
+++ Eclipse_WorkSpace/facebook/src/main/java/controller/UsersController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/"})
+public class UsersController extends HttpServlet{
+
+	
+	
+}
